158639b11554cca8fd38c9d17bb089a247f388d4
Chirag Nagpal <nagpalchirag1994@gmail.com>
2022-06-16 13:53:20 -0400

    modified:   dev/dev_synthetic_data.py

--- dev/dev_synthetic_data.py
+++ dev/dev_synthetic_data.py
@@ -4,9 +4,17 @@ sys.path.append('./')
 from urp import dataset
 
 data_generator = dataset.CensoredSurvivalData()
+data_generator.plot_sample(n=10000)
 
-sample = data_generator.sample(n=10000)
 
+data_generator = dataset.CensoredSurvivalData(censoring_dist_scale=10)
+data_generator.plot_sample(n=10000)
+
+
+data_generator = dataset.CensoredSurvivalData(censoring_dist_min_scale=2)
+data_generator.plot_sample(n=10000)
+
+data_generator = dataset.CensoredSurvivalData(censoring_dist_maj_loc=2, censoring_dist_min_scale=8)
 data_generator.plot_sample(n=10000)
 
 pass
